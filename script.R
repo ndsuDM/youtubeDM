e2d23e21f8777673f2fa49702f85f14933dee32b
raboin09 <raboin09@gmail.com>
2017-12-05 12:25:04 -0600

    adjusted

--- script.R
+++ script.R
@@ -1,7 +1,6 @@
 library("igraph")
 
-print("gets here")
 dd<-as.matrix(read.table("~/Documents/DM/com-youtube.ungraph.txt"))
 graph_from_edgelist(dd, directed=FALSE)
 gg<-graph_from_edgelist(cbind(dd))
-vertex_attr(gg, "1")
+E(gg) [from(1)]
