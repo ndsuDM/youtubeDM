01eed279b43b4ceac782490bf8a66c458788834a
raboin09 <raboin09@gmail.com>
2017-12-05 16:03:53 -0600

    New files

--- script.R
+++ script.R
@@ -1,10 +1,9 @@
-#install.packages("igraph")
+install.packages("igraph")
 library("igraph")
-
+print("start")
 #dd<-as.matrix(read.table("~/Documents/DM/com-youtube.ungraph.txt"))
-dd<-as.matrix(read.table("~/Documents/DM/test.txt"))
+dd<-as.matrix(read.table("~/Documents/DM/graphV1000.txt"))
+dd<-dd+1
 graph_from_edgelist(dd, directed = FALSE)
 gg <- graph_from_edgelist(cbind(dd), directed = FALSE)
-print(E(gg) [from(1)])
 ebc <- cluster_edge_betweenness(gg)
-print(ebc)
